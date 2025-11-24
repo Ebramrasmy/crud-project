03a757d42db2ab738ce080cf4e35f26440cd7cbd
Ebram Rasmy <ebramrasmy7@gmil.com>
2025-11-24 15:58:02 +0200

    create function display

--- js/script.js
+++ js/script.js
@@ -3,12 +3,41 @@ var productPriceInput = document.getElementById("productPrice");
 var productCategoryInput = document.getElementById("productCategory");
 var productDescInput = document.getElementById("productDescription");
 
+var productcontainer=[];
+
 function addProduct() {
-    var oroduct={
+    var product={
         name: productNameInput.value,
         price: productPriceInput.value,
         category: productCategoryInput.value,
         description: productDescInput.value
     }
-    console.log(oroduct);
+    productcontainer.push(product);
+    console.log(productcontainer);
+    clearForm();
+    displayProducts();
+
+}
+
+function clearForm() {
+    productNameInput.value = "";
+    productPriceInput.value = "";
+    productCategoryInput.value = "";
+    productDescInput.value = "";
+}
+
+function displayProducts() {
+    var cartoona = "";
+    for (var i = 0; i < productcontainer.length; i++) {
+        cartoona += `<tr>
+        <th scope="row">${i+1}</th>
+        <td>${productcontainer[i].name}</td>
+        <td>${productcontainer[i].price}</td>
+        <td>${productcontainer[i].category}</td>
+        <td>${productcontainer[i].description}</td>
+        <td><button class="btn btn-outline-danger">Delete</button></td>
+        <td><button class="btn btn-outline-warning">Update</button></td>
+      </tr>`;
+    }
+document.getElementById("productTableBody").innerHTML = cartoona;
 }
